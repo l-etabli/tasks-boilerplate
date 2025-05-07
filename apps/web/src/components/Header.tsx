41e0232fe8544522e8a52f717afccee8aff98d39
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-05-07 22:18:03 +0200

    remove useless routes

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -15,22 +15,6 @@ export default function Header() {
         <div className="px-2 font-bold">
           <Link to="/demo/start/server-funcs">Start - Server Functions</Link>
         </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/start/api-request">Start - API Request</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/form/simple">Simple Form</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/form/address">Address Form</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/store">Store</Link>
-        </div>
       </nav>
     </header>
   );
