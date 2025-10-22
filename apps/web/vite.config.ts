4674593c5e474877b37157743037ca248008e25c
Jérôme Burkard <22095555+JeromeBu@users.noreply.github.com>
2025-10-22 14:48:35 +0200

    fix deployment

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
+    nitroV2Plugin({ preset: 'node-server' }),
     tanstackStart(),
     viteReact(),
   ],
